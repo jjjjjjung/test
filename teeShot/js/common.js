89a9b4dcb765f5b079a9c11ea4a64bc168240afb
jjung-park <jj112490@gmail.com>
2022-05-30 01:23:26 +0900

    terms

--- teeShot/js/common.js
+++ teeShot/js/common.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     scrollHeader();
     scrollIcon();
-    tabNav()
+    tabNav();
+    mobileNav();
 
 });
 var mobile = 769 >= window.innerWidth
@@ -43,7 +44,7 @@ function scrollIcon(params) {
 function tabNav() {
     $(".tabArea .tab li a").on("click", function () {
         // 해당 요소를 클릭하는 내 자신의 index 번호를 가져온다. [0], [1]
-        const num = $(".tabArea .tab li a").index($(this));
+        let num = $(".tabArea .tab li a").index($(this));
 
         // 기존에 적용되어 있는 on class 삭제
         $(".tabArea .tab li").removeClass("on");
@@ -54,4 +55,30 @@ function tabNav() {
         $('.tabArea .tabBox:eq(' + num + ')').addClass("on");
 
     });
+};
+
+function mobileNav(params) {
+    $('#select_menu a').on('click', function () {
+        $('.sub_tab').toggleClass('active');
+        $('#select_menu a').toggleClass('on');
+        $('.dismiss').toggleClass('on')
+    })
+    $('.sub_tab li a').on('click', function () {
+        let num = $(".sub_tab li a").index($(this));
+
+        $('#select_menu a').text($(this).text());
+
+
+        // 기존에 적용되어 있는 on class 삭제
+        $('.sub_tab').removeClass('active')
+        $('#select_menu a').removeClass('on');
+
+        $(".sub_tab li").removeClass("on");
+        $(".tabArea .tabBox").removeClass("on");
+
+        // 다음 요소 클릭시 on class 추가
+        $('.sub_tab li:eq(' + num + ')').addClass("on");
+        $('.tabArea .tabBox:eq(' + num + ')').addClass("on");
+    });
+
 }
